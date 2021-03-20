d77c16dc783a59244094f8231717aa4454035ec9
Klaus Hörmann-Engl <kho@world-direct.at>
2021-03-20 20:47:06 +0100

    [TASK] Add status info in client list

--- Classes/Domain/Repository/TaskRepository.php
+++ Classes/Domain/Repository/TaskRepository.php
@@ -31,4 +31,59 @@ class TaskRepository extends BaseRepository
         $tasks = $query->execute()->toArray();
         return $tasks;
     }
+
+    /**
+     * Method returns the overall task status for a given client.
+     * Depending on the Function checks the overall task status.
+     * Depending on the tasks following return values are valid:
+     * - 0: All tasks are ok
+     * - 1: At least 1 task is late, no task is failing
+     * - 2: At least 1 task is failing
+     *
+     * @param integer $clientId
+     * @return void
+     */
+    public function getTaskStatusForClientId(int $clientId)
+    {
+        $lateCount = 0;
+        $errorCount = 0;
+
+        $query = $this->getQuery();
+        $query->matching(
+            $query->logicalAnd(
+                $query->equals('client', intval($clientId)),
+                $query->logicalOr([
+                    $query->equals('late', 1),
+                    $query->logicalNot($query->equals('lastexecution_failure', ''))
+                ])
+            )
+        );
+        $errorTasks = $query->execute()->toArray();
+
+        if ($errorTasks) {
+            foreach ($errorTasks as $errorTask) {
+                if ($errorTask->getLate() == 1) {
+                    $lateCount++;
+                }
+                if ($errorTask->getLastexecutionFailure() != '') {
+                    $errorCount++;
+                }
+            }
+
+            if ($errorCount) {
+                return 2; // Overall: Error
+            }
+    
+            if ($lateCount) {
+                return 1; // Overall: Warning
+            }
+        } else {
+            $query = $this->getQuery();
+            $taskCount = $query->matching($query->equals('client', intval($clientId)))->count();
+            if ($taskCount) {
+                return 3; // Overall: Ok
+            }
+        }
+        return 0; // OVerall: Empty
+    }
 }
