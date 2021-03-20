3c45df43ffbe668d5c9c389a73eae64079a03b5b
Klaus Hörmann-Engl <kho@world-direct.at>
2021-03-20 17:08:39 +0100

    [TASK] Add findByClientId repo method

--- Classes/Domain/Repository/TaskRepository.php
+++ Classes/Domain/Repository/TaskRepository.php
@@ -1,6 +1,8 @@
 <?php
 namespace T3Monitor\T3monitoring\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
 /*
  * This file is part of the t3monitoring extension for TYPO3 CMS.
  *
@@ -10,4 +12,23 @@ namespace T3Monitor\T3monitoring\Domain\Repository;
 
 class TaskRepository extends BaseRepository
 {
+    public function initializeObject(): void
+    {
+        $this->setDefaultOrderings(['client' => QueryInterface::ORDER_ASCENDING]);
+    }
+
+    /**
+     * Function returns all tasks for a specific clientId
+     *
+     * @param integer $clientId The id of the desired client
+     *
+     * @return array<array> The tasks of this client
+     */
+    public function findByClientId(int $clientId)
+    {
+        $query = $this->getQuery();
+        $query->matching($query->equals('client', intval($clientId)));
+        $tasks = $query->execute()->toArray();
+        return $tasks;
+    }
 }
