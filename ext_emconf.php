c8208f8dd42e9015d2fa4ec252aae1843d850b33
Markus Klein <markus.klein@reelworx.at>
2024-08-03 00:08:08 +0200

    [BUGFIX] Disallow v11 in emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.4.99',
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
